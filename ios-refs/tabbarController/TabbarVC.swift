bbcde9e90ca1fe49b33511098fdc08b352113bba
Leon Wee <leon.wee.yuan.liang@accenture.com>
2021-11-03 22:00:56 +0800

    Tabbar and navigation controller

---
+++ ios-refs/tabbarController/TabbarVC.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarVC.swift
+//  ios-refs
+//
+//  Created by leon.wee.yuan.liang on 3/11/21.
+//
+
+import UIKit
+
+class TabbarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
